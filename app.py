9701937699d622ea2626bb88aadc4613758fa7d0
Donald McLaughlin <donald_j97@hotmail.com>
2023-10-03 10:02:07 -0400

    New: pulls from boostrap template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from flask_bootstrap import Bootstrap
 
@@ -6,12 +6,12 @@ def create_app():
   app = Flask(__name__)
   Bootstrap(app)
 
+  return app
 
-app = Flask(__name__)
-
+app = create_app()
 @app.route("/")
 def hello_world():
-    return "<p>I am using flask and bootstrap Hello!</p>"
+    return render_template('my_template.html')
 
 if __name__ == '__main__':
     app.run()
